449aa88920914a61236c727481a807b26a2edf62
Spencer <spencerhorn@gmail.com>
2025-12-04 07:41:39 -0500

    updated svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,9 @@
 import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
+
 	kit: {
 		adapter: adapter()
 	}
